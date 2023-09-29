18beeb94b876c4832166abfa9524a3806aa89fb0
Sergey Morozov <globalvpsm@gmail.com>
2023-09-30 01:24:17 +0300

    syntax err...

--- project/ui/user_interaction.py
+++ project/ui/user_interaction.py
@@ -1,3 +1,3 @@
 name=input("Hello, whats your name?")
-board_size=int(input("please, choose board size:"))
+board_size=int(input("Michael, please choose board size:"))
 number_of_mine=int(input("choose number of mines to allocate:"))
